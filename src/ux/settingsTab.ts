febceb123ba17334d30ead984260d6a49868ac37
TfT Hacker <blockreference.com>
2022-09-20 06:59:37 +0200

    Updated settings screen with tooltip count

--- src/ux/settingsTab.ts
+++ src/ux/settingsTab.ts
@@ -1,11 +1,12 @@
-import { App, PluginSettingTab, Setting, ToggleComponent } from 'obsidian';
-import ThePlugin from '../main';
+import { App, PluginSettingTab, Setting, ToggleComponent } from "obsidian";
+import ThePlugin from "../main";
 
 export interface Settings {
 	displayIncomingFilesheader: 	boolean;
 	displayInlineReferences: 		boolean;
 	displayEmbedReferencesInGutter:	boolean;
 	displayLineNumberInSidebar:		boolean;
+	displayNumberOfFilesInTooltip:	number;
 	cacheUpdateInMilliseconds:		number;
 }
 
@@ -14,6 +15,7 @@ export const DEFAULT_SETTINGS: Settings = {
 	displayInlineReferences: true,
 	displayEmbedReferencesInGutter: true,
 	displayLineNumberInSidebar: true,
+	displayNumberOfFilesInTooltip: 10,
 	cacheUpdateInMilliseconds: 10000
 }
 
@@ -32,8 +34,8 @@ export class SettingsTab extends PluginSettingTab {
 		containerEl.createEl("h2", { text: "Display elements" });
 
 		new Setting(containerEl)
-			.setName('Incoming Links Header Count')
-			.setDesc('In header of a document, show number of incoming link to that file.')
+			.setName("Incoming Links Header Count")
+			.setDesc("In header of a document, show number of incoming link to that file.")
 			.addToggle((cb: ToggleComponent) => {
 				cb.setValue(this.thePlugin.settings.displayIncomingFilesheader);
 				cb.onChange(async (value: boolean) => {
@@ -43,8 +45,8 @@ export class SettingsTab extends PluginSettingTab {
 			});
 
 		new Setting(containerEl)
-			.setName('References Inline Documents')
-			.setDesc('Display inline of the text of documents all reference counts for links, blocks and embeds. (Requires Obsidian Restart)')
+			.setName("References Inline Documents")
+			.setDesc("Display inline of the text of documents all reference counts for links, blocks and embeds. (Requires Obsidian Restart)")
 			.addToggle((cb: ToggleComponent) => {
 				cb.setValue(this.thePlugin.settings.displayInlineReferences);
 				cb.onChange(async (value: boolean) => {
@@ -55,7 +57,7 @@ export class SettingsTab extends PluginSettingTab {
 
 
 		new Setting(containerEl)
-			.setName('Embed references in Gutter in Live Preview Mode  (Requires Obsidian Restart)')
+			.setName("Embed references in Gutter in Live Preview Mode  (Requires Obsidian Restart)")
 			.setDesc(`Displays a count of references in the gutter while in live preview. This is done only in a
 					  special scenario. It has to do with the way Obsidian renders embeds, example: ![[link]] when  
 					  they are on its own line. Strange New Worlds cannot embed the count in this scenario, so a hint is 
@@ -69,8 +71,22 @@ export class SettingsTab extends PluginSettingTab {
 			});
 
 		new Setting(containerEl)
-			.setName('Show line number for file in sidepane')
-			.setDesc(`Displays a line number from the document in the sidepane.`	)
+			.setName("Number of files listed in tooltip")
+			.setDesc(`The block reference count shows a tooltip with files for the block reference. This settting controls the count of files listed.
+					  Set this to 0 for no tooltip. Currently set to: ${this.thePlugin.settings.displayNumberOfFilesInTooltip} files.`)
+			.addSlider(slider => slider
+				.setLimits(0, 30 , 1)
+				.setValue(this.thePlugin.settings.displayNumberOfFilesInTooltip)
+				.onChange(async (value) => {
+					this.thePlugin.settings.displayNumberOfFilesInTooltip = value;
+					await this.thePlugin.saveSettings();
+				})
+				.setDynamicTooltip()
+			)
+
+		new Setting(containerEl)
+			.setName("Show line number for file in sidepane")
+			.setDesc("Displays a line number from the document in the sidepane.")
 			.addToggle((cb: ToggleComponent) => {
 				cb.setValue(this.thePlugin.settings.displayLineNumberInSidebar);
 				cb.onChange(async (value: boolean) => {
@@ -81,7 +97,7 @@ export class SettingsTab extends PluginSettingTab {
 			
 			containerEl.createEl("h2", { text: "Cache Tuning" });
 
-			new Setting(containerEl)
+		new Setting(containerEl)
 			.setName(`How often should the SNW Cache update`)
 			.setDesc(`By default SNW will updates its internal cache every 10 seconds (10,000 milliseconds) when there is some change in the vualt.
 					  Increase the time to slighlty improve performance or decrease it to improve refresh of vault information.
